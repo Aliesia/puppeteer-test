7ebc9c752ece0fa2828be90d43c1f8e584d2b879
Aliesia Pavliuk <alesia.lexy@gmail.com>
2020-05-15 23:44:00 +0200

    add war log initial parcing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {getPageContent} from './helpers/puppeteer.js';
 
 
 const SITE = 'https://royaleapi.com/clan/89VLQR0';
-
+const WARPAGE = 'https://royaleapi.com/clan/89VLQR0/war/analytics';
 
 
 (async () => {
@@ -24,7 +24,17 @@ const SITE = 'https://royaleapi.com/clan/89VLQR0';
       })
     })
 
-    console.log(clanMembers);
+    let warContent = await getPageContent(WARPAGE);
+
+    $.load(warContent);
+    $ = cherio.load(warContent);
+
+    $('.war_table_wrapper tbody .war_member').each((i, contentRowLink) => {
+      let tag = $(contentRowLink).attr('data-tag');
+      let mia = $(contentRowLink).find('td:nth-of-type(4)').attr('data-sort-value');
+
+    })
+    
 
   }catch (error){
     console.log(chalk.red('error: \n'));
