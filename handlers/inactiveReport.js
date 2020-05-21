60d734455696b0282c0754668c72d3157fbf674f
Aliesia Pavliuk <alesia.lexy@gmail.com>
2020-05-21 17:51:23 +0200

    add tag to inactive member

--- handlers/inactiveReport.js
+++ handlers/inactiveReport.js
@@ -5,7 +5,7 @@ export default async function inactiveMembersReport(data){
   
     for (let [key, value] of Object.entries(data)){
         if(!('mia' in value) && !('join_status' in value)){
-          inactiveMembers.push(value.name);
+          inactiveMembers.push([value.name, value.tag]);
         }
     }
     
