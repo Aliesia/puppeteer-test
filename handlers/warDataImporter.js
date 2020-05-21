2ce961504226cc093f4b034ad076d869beb76db1
Aliesia Pavliuk <alesia.lexy@gmail.com>
2020-05-21 17:13:53 +0200

    add war battles score

--- handlers/warDataImporter.js
+++ handlers/warDataImporter.js
@@ -11,13 +11,20 @@ export default async function warDataImporter(clanMembers){
     $('.war_table_wrapper tbody .war_member').each((i, contentRowLink) => {
       let tag = $(contentRowLink).attr('data-tag');
       let mia = $(contentRowLink).find('td:nth-of-type(4)').attr('data-sort-value');
+      let lastBattlesScore =
+      parseInt($(contentRowLink).find('td:nth-child(5)').attr('data-sort-value')) + 
+      parseInt($(contentRowLink).find('td:nth-child(6)').attr('data-sort-value')) + 
+      parseInt($(contentRowLink).find('td:nth-child(7)').attr('data-sort-value'));
+
 
       if (!clanMembers[tag]) {
         return;
       }
 
       clanMembers[tag].mia = mia;
-      
+      clanMembers[tag].lastBattlesScore = lastBattlesScore;
+
     })
+
     return clanMembers;
 }
