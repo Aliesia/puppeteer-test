3a7c021ef0b8d805f5bdf8866913a697bb923a14
Aliesia Pavliuk <alesia.lexy@gmail.com>
2020-05-24 23:25:06 +0200

    style correction

--- helpers/telegram.js
+++ helpers/telegram.js
@@ -25,44 +25,62 @@ bot.on('message', async (msg) => {
     } 
 
     if (msg.text.toString().toLowerCase().indexOf('хай') === 0) {
-        await bot.sendMessage(msg.chat.id,'Привіт ' + msg.from.first_name + emoji.wave + ' \n ' + 
-        'Доступні команди:' + ' \n ' + 
-        '<b> топ </b>' + ' \n ' +
-        '-показати тих, що перемогли в 3х останніх кв' + ' \n ' +
-        '<b> пасивні </b> ' + ' \n ' + 
-        '-показати тих, що не зіграли в останніх 10 війнах' + ' \n ' +
-        '<b> пропустили </b> ' + ' \n ' + 
-        '-показати тих, що не зіграли в останніх 3х війнах' + ' \n ' +
-        '<b> обраний </b> ' + ' \n ' + 
-        '-показати гравця з найкращим рейтингом у клан війнах' + ' \n ', {parse_mode : "HTML"}
+        await bot.sendMessage(msg.chat.id,'Привіт ' + msg.from.first_name + emoji.wave + ' \n' + 
+        'Доступні команди:' + ' \n' + 
+        '<b> топ </b>' + ' \n' +
+        '-показати тих, що перемогли в 3х останніх кв' + ' \n' +
+        '<b> пасивні </b> ' + ' \n' + 
+        '-показати тих, що не зіграли в останніх 10 війнах' + ' \n' +
+        '<b> пропустили </b> ' + ' \n' + 
+        '-показати тих, що не зіграли в останніх 3х війнах' + ' \n' +
+        '<b> обраний </b> ' + ' \n' + 
+        '-показати гравця з найкращим рейтингом у клан війнах' + ' \n', {parse_mode : "HTML"}
         )
         
     } 
 
     if (msg.text.toString().toLowerCase().indexOf('топ') === 0) {
         let topBattleMembers = await topPlayersRank(warData);
-        await bot.sendMessage(msg.chat.id,'Players win in last 3 wars' + emoji.confetti_ball);
-        await bot.sendMessage(msg.chat.id, topBattleMembers.join(' \n '));
-        await bot.sendMessage(msg.chat.id,'Total: ' + topBattleMembers.length);
-    } 
+        if (topBattleMembers.length != 0){
+            let topBattleMembers = await topPlayersRank(warData);
+            await bot.sendMessage(msg.chat.id,'Гравці, які виграли у 3х останніх клан війнах' + emoji.confetti_ball + ':' + ' \n' + ' \n'+
+            topBattleMembers.join(' \n') + ' \n' + 
+            '<b>Всього:</b> ' + topBattleMembers.length, {parse_mode : "HTML"});
+        } else {
+            await bot.sendMessage(msg.chat.id,'Шукав усюди, але не вдалось знайти жодного такого гравця' + emoji.sad)
+        }
+    }
+
     
     if (msg.text.toString().toLowerCase().indexOf('пасивні') === 0) {
         let inactiveMembers = await inactiveMembersReport(warData);
-        await bot.sendMessage(msg.chat.id,'Players without participation in last 10 wars' + emoji.crying_cat_face);
-        await bot.sendMessage(msg.chat.id, inactiveMembers.join(' \n '));
-        await bot.sendMessage(msg.chat.id,'Total: ' + inactiveMembers.length);
-    } 
+        if (inactiveMembers.length != 0){
+            await bot.sendMessage(msg.chat.id,'Гравці, які не брали участь у 10 останніх кв' + emoji.scream + ':' +' \n' + ' \n' +
+            inactiveMembers.join(' \n') + ' \n' + 
+            '<b>Всього: </b> '  + inactiveMembers.length, {parse_mode : "HTML"});
+        } else {
+            await bot.sendMessage(msg.chat.id,'Ура! Не знайдено жодного такого гравця!' + emoji.happy)
+        }
+    }
 
     if (msg.text.toString().toLowerCase().indexOf('пропустили') === 0) {
         let missBattleMembers = await missBattlePlayers(warData);
-        await bot.sendMessage(msg.chat.id,'Players not participating in last 3 wars' + emoji.angry);
-        await bot.sendMessage(msg.chat.id, missBattleMembers.join(' \n '));
-        await bot.sendMessage(msg.chat.id,'Total: ' + missBattleMembers.length);
-    } 
+        if (missBattleMembers.length != 0){
+            await bot.sendMessage(msg.chat.id,'Гравці, які не взяли участь у 3х останніх кв' + emoji.angry + ':' + ' \n' + ' \n' +
+            missBattleMembers.join(' \n') + ' \n' + 
+            '<b>Всього:</b> '  + missBattleMembers.length, {parse_mode : "HTML"});
+        } else {
+            await bot.sendMessage(msg.chat.id,'Здивувався, але не знайшов жодного такого гравця' + emoji.happy)
+        }
+    }
 
     if (msg.text.toString().toLowerCase().indexOf('обраний') === 0) {
         let topPlayer = await bestWinRate(warData);
-        await bot.sendMessage(msg.chat.id,'Best player ever is ' + topPlayer.name + emoji.heart);
+        if (topPlayer != false){
+            await bot.sendMessage(msg.chat.id,'Гравець з найкращим рейтингом у клан війнах' + emoji.sunny + '=>' + ' \n' + '<b>' + topPlayer.name +'</b>', {parse_mode : "HTML"});
+        } else{
+            await bot.sendMessage(msg.chat.id,'Не знайдено достойного гравця' + emoji.cry)
+        }
     } 
     
 });
